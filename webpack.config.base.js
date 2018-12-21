1153905d8373e663baaf9d0764c27dfc04293a75
Juan Luis Rojas León <rojasleon122@gmail.com>
2018-12-21 15:08:52 -0600

    create separate webpack configs for development and production

--- webpack.config.base.js
+++ webpack.config.base.js
@@ -1,6 +1,7 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
 module.exports = {
-  mode: 'development',
   entry: './src/index.js',
   output: {
     path: path.join(__dirname, 'dist'),
@@ -17,5 +18,8 @@ module.exports = {
         }
       }
     ]
-  }
+  },
+  plugins: [new HtmlWebpackPlugin({
+    template: './src/index.html'
+  })]
 };
