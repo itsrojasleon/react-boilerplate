c4b37bb601a61f2e651c068f722ae65ef3d9752a
Juan Luis Rojas León <rojasleon122@gmail.com>
2018-12-21 19:07:48 -0600

    global test

--- src/App.spec.js
+++ src/App.spec.js
@@ -1,7 +1,5 @@
 import React from 'react';
 import { render } from 'react-testing-library';
-import 'jest-dom/extend-expect';
-import 'react-testing-library/cleanup-after-each';
 import App from './App';
 
 describe('App', () => {
