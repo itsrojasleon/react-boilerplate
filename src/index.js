92b5df07e0f60da435128579188bfd01e6298b14
Juan Luis Rojas León <rojasleon122@gmail.com>
2018-12-21 18:16:45 -0600

    Add react and react-dom via CDN

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
-import { render } from 'react-dom';
+import ReactDOM from 'react-dom';
 import App from './App';
 import './styles.css';
 
-render(
+ReactDOM.render(
   <App />,
   document.getElementById('app')
 );
