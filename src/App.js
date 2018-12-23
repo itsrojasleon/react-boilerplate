81033f3e5702803194d4349797caa2d63a772718
Juan Luis Rojas León <rojasleon122@gmail.com>
2018-12-23 17:49:08 -0600

    cleanup

--- src/App.js
+++ src/App.js
@@ -1,34 +1,11 @@
 import React from 'react';
 import { hot } from 'react-hot-loader';
 
-const Warning = React.lazy(() => import('./Warning'));
-
 class App extends React.Component {
-  state = {
-    count: 0
-  };
-  increment = () => {
-    this.setState(state => ({ count: state.count + 1 }));
-  };
-  decrement = () => {
-    this.setState(state => ({ count: state.count - 1 }));
-  };
   render() {
     return (
       <div>
-        <h1 className="title">Hello</h1>
-        <button onClick={this.increment}>+</button>
-        <button onClick={this.decrement}>-</button>
-        <h2>
-          Count:{' '}
-          {this.state.count > 10 ? (
-            <React.Suspense fallback={null}>
-              <Warning />
-            </React.Suspense>
-          ) : (
-            <div>{this.state.count}</div>
-          )}
-        </h2>
+        <h1 className="title">Hello World!</h1>
       </div>
     );
   }
